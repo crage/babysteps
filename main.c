59a0ae394a65c87e8c928ff2809fddfcdad58580
crage <imcrage@mail.ru>
2013-07-25 22:11:12 +0400

    test again

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include "stdio.h"
 
 int main(void){
+	char name[255];
+	printf ("Hello! Enter your name: ");
+	scanf ("%s", name);
+	
+	printf ("Thanks. Your name is '%s'.\n", name);
 	return 1;
 }
